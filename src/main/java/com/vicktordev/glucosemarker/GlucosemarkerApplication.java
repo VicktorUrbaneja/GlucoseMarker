c8a431850757bf0fae811aae5579d896e93a01a4
Vicktor <vicktorurbaneja@hotmail.com>
2024-12-13 17:01:17 -0300

    commit inicial do projeto

---
+++ src/main/java/com/vicktordev/glucosemarker/GlucosemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.vicktordev.glucosemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucosemarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlucosemarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicktordev/glucosemarker/GlucosemarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicktordev.glucosemarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucosemarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
